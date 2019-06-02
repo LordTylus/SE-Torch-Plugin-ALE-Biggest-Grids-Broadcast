0c8a74060fe1597980da7efff00108d920c39fe2
LordTylus <LordTylus@users.noreply.github.com>
2019-06-02 21:55:24 +0200

    List Command improvements

--- Commands.cs
+++ Commands.cs
@@ -31,12 +31,13 @@ namespace ALE_Biggest_Grids_Broadcast {
 
             Plugin.removeGpsFromAllPlayers();
 
-            List<KeyValuePair<long, List<MyCubeGrid>>> grids = FindGrids();
-            List<KeyValuePair<long, List<MyCubeGrid>>> filteredGrids = GetFilteredGrids(grids);
+            List<KeyValuePair<long, List<MyCubeGrid>>> grids = FindGrids(Plugin.UseConnectedGrids);
+            List<KeyValuePair<long, List<MyCubeGrid>>> filteredGrids = GetFilteredGrids(grids, 
+                Plugin.MinPCU, Plugin.MaxDistancePlayers, Plugin.TopGrids, true);
 
             List<MyGps> gpsList = new List<MyGps>();
 
-            double seconds = (long) (DateTime.Now.ToUniversalTime() - new DateTime(1970, 1, 1)).TotalMilliseconds;
+            long seconds = getTimeMs();
 
             int i = 0;
 
@@ -50,16 +51,7 @@ namespace ALE_Biggest_Grids_Broadcast {
 
                 var position = grid.PositionComp.GetPosition();
 
-                MyGps gps = new MyGps();
-                gps.Coords = grid.PositionComp.GetPosition();
-                gps.Name = "Top Grid - " + grid.DisplayName+ " " +seconds;
-                gps.DisplayName = "Top Grid: " + grid.DisplayName;
-                gps.Description = ($"Top Grid: Grid currently in Top {i} by {Plugin.GpsIdentifierName}");
-                gps.GPSColor = gpsColor;
-                gps.IsContainerGPS = true;
-                gps.ShowOnHud = true;
-                gps.DiscardAt = new TimeSpan?();
-                gps.UpdateHash();
+                MyGps gps = createGps(i, grid, gpsColor, seconds);
 
                 gpsList.Add(gps);
             }
@@ -84,24 +76,72 @@ namespace ALE_Biggest_Grids_Broadcast {
         [Permission(MyPromoteLevel.Moderator)]
         public void ListBiggestGrids() {
 
-            List<KeyValuePair<long, List<MyCubeGrid>>> grids = FindGrids();
-            List<KeyValuePair<long, List<MyCubeGrid>>> filteredGrids = GetFilteredGrids(grids);
+            int top = Plugin.TopGrids;
+            int distance = Plugin.MaxDistancePlayers;
+            int min = Plugin.MinPCU;
+            bool connected = Plugin.UseConnectedGrids;
+            bool filterOffline = true;
+            bool gps = false;
+
+            List<string> args = Context.Args;
+            foreach(string arg in args) {
+
+                if (arg == "-phsical")
+                    connected = true;
+
+                if (arg == "-mechanical")
+                    connected = false;
+
+                if (arg == "-gps")
+                    gps = true;
+
+                if (arg == "-showOffline")
+                    filterOffline = false;
+
+                if (arg.StartsWith("-top=")) {
+
+                    string localArg = arg.Replace("-top=", "");
+                    int.TryParse(localArg, out top);
+                }
+
+                if (arg.StartsWith("-min=")) {
+
+                    string localArg = arg.Replace("-min=", "");
+                    int.TryParse(localArg, out min);
+                }
+
+                if (arg.StartsWith("-distance=")) {
+
+                    string localArg = arg.Replace("-distance=", "");
+                    int.TryParse(localArg, out distance);
+                }
+            }
+
+            List<KeyValuePair<long, List<MyCubeGrid>>> grids = FindGrids(connected);
+            List<KeyValuePair<long, List<MyCubeGrid>>> filteredGrids = GetFilteredGrids(grids, min, distance, top, filterOffline);
 
             StringBuilder sb = new StringBuilder();
             sb.AppendLine("Used Settings");
             sb.AppendLine("---------------------------------------");
-            sb.AppendLine("Number of grids: " + Plugin.TopGrids);
-            sb.AppendLine("Player distance: " + Plugin.MaxDistancePlayers);
-            sb.AppendLine("Min PCU: "+ Plugin.MinPCU);
-            sb.AppendLine("Include: "+ (!Plugin.UseConnectedGrids ? "Phyiscal Connections (Rotors, Pistons)" : "Mechanical Connections (Rotors, Pistons, Connectors)"));
+            sb.AppendLine("Number of grids: " + top);
+            sb.AppendLine("Player distance: " + distance);
+            sb.AppendLine("Min PCU: "+ min);
+            sb.AppendLine("Include: "+ (!connected ? "Phyiscal Connections (Rotors, Pistons)" : "Mechanical Connections (Rotors, Pistons, Connectors)"));
             sb.AppendLine();
 
             sb.AppendLine("Result");
             sb.AppendLine("---------------------------------------");
 
+            long seconds = getTimeMs();
+
             int i = 0;
 
-            foreach(KeyValuePair<long, List<MyCubeGrid>> pair in filteredGrids) {
+            Color gpsColor = Plugin.GpsColor;
+
+            if (gps && Context.Player != null)
+                Plugin.removeGpsFromPlayer(Context.Player.IdentityId);
+
+            foreach (KeyValuePair<long, List<MyCubeGrid>> pair in filteredGrids) {
 
                 i++;
 
@@ -120,34 +160,58 @@ namespace ALE_Biggest_Grids_Broadcast {
                     }
                 }
 
+                var gridOwnerList = biggestGrid.BigOwners;
+                var ownerCnt = gridOwnerList.Count;
+                var gridOwner = 0L;
+
+                if (ownerCnt > 0 && gridOwnerList[0] != 0)
+                    gridOwner = gridOwnerList[0];
+                else if (ownerCnt > 1)
+                    gridOwner = gridOwnerList[1];
+
+                IMyFaction faction = GetFactionForPlayer(gridOwner);
+
+                String factionString = "";
+                if (faction != null)
+                    factionString = "[" + faction.Tag + "]";
+
+
                 sb.AppendLine(i+". "+pair.Key.ToString("#,##0") + " PCU"+" - "+ biggestGrid.DisplayName);
                 sb.AppendLine("   "+pair.Value.Count +" Grids.");
+                sb.AppendLine("   Owned by: " + GetPlayerNameById(gridOwner) +" "+ factionString);
 
                 MyCubeGrid grid = pair.Value[0]; /* Cannot be empty because where do the PCUs come from? */
 
                 var position = grid.PositionComp.GetPosition();
 
                 sb.AppendLine($"   X: {position.X.ToString("#,##0.00")}, Y: {position.Y.ToString("#,##0.00")}, Z: {position.Z.ToString("#,##0.00")}");
+
+                if (gps && Context.Player != null) {
+
+                    MyGps gridGPS = createGps(i, grid, gpsColor, seconds);
+
+                    MyAPIGateway.Session?.GPS.AddGps(Context.Player.IdentityId, gridGPS);
+                }
             }
 
             if (Context.Player == null) {
 
-                Context.Respond("Top " + Plugin.TopGrids + " grids by PCU");
+                Context.Respond("Top " + top + " grids by PCU");
                 Context.Respond(sb.ToString());
 
             } else {
 
-                ModCommunication.SendMessageTo(new DialogMessage("List of Biggest Grids", "Top "+Plugin.TopGrids+" grids by PCU", sb.ToString()), Context.Player.SteamUserId);
+                ModCommunication.SendMessageTo(new DialogMessage("List of Biggest Grids", "Top "+top+" grids by PCU", sb.ToString()), Context.Player.SteamUserId);
             }
         }
 
-        public List<KeyValuePair<long, List<MyCubeGrid>>> FindGrids() {
+        public List<KeyValuePair<long, List<MyCubeGrid>>> FindGrids(bool connected) {
 
             StringBuilder sb = new StringBuilder();
 
             List<KeyValuePair<long, List<MyCubeGrid>>> gridsList = new List<KeyValuePair<long, List<MyCubeGrid>>>();
 
-            if (Plugin.UseConnectedGrids) {
+            if (connected) {
 
                 foreach (var group in MyCubeGridGroups.Static.Physical.Groups)
                     gridsList.Add(CheckGroupsPcu(group.Nodes));
@@ -205,7 +269,8 @@ namespace ALE_Biggest_Grids_Broadcast {
             return new KeyValuePair<long, List<MyCubeGrid>>(pcu, gridsList);
         }
 
-        private List<KeyValuePair<long, List<MyCubeGrid>>> GetFilteredGrids(List<KeyValuePair<long, List<MyCubeGrid>>> sortedGrids) {
+        private List<KeyValuePair<long, List<MyCubeGrid>>> GetFilteredGrids(
+            List<KeyValuePair<long, List<MyCubeGrid>>> sortedGrids, int min, int distance, int top, bool filterOffline) {
 
             List<KeyValuePair<long, List<MyCubeGrid>>> gridsList = new List<KeyValuePair<long, List<MyCubeGrid>>>();
 
@@ -215,24 +280,24 @@ namespace ALE_Biggest_Grids_Broadcast {
                     continue;
 
                 /* Too little PCU ignore */
-                if (pair.Key < Plugin.MinPCU)
+                if (pair.Key < min)
                     continue;
 
-                bool relevant = CheckIfGridsAreRelevant(pair.Value);
+                bool relevant = CheckIfGridsAreRelevant(pair.Value, distance, filterOffline);
 
                 if (relevant)
                     gridsList.Add(pair);
 
-                if (gridsList.Count == Plugin.TopGrids)
+                if (gridsList.Count == top)
                     break;
             }
 
             return gridsList;
         }
 
-        private bool CheckIfGridsAreRelevant(List<MyCubeGrid> grids) {
+        private bool CheckIfGridsAreRelevant(List<MyCubeGrid> grids, int distance, bool filterOffline) {
 
-            int minDistanceSquared = Plugin.MaxDistancePlayers * Plugin.MaxDistancePlayers;
+            int minDistanceSquared = distance * distance;
 
             foreach (MyCubeGrid grid in grids) {
 
@@ -257,8 +322,11 @@ namespace ALE_Biggest_Grids_Broadcast {
                     foreach(long factionMember in faction.Members.Keys) {
 
                         MyPlayer member = GetPlayerById(factionMember);
-                        if (member == null)
+                        if (filterOffline && member == null)
                             continue;
+
+                        if (!filterOffline)
+                            return true;
 
                         Vector3D position = member.GetPosition();
 
@@ -282,6 +350,35 @@ namespace ALE_Biggest_Grids_Broadcast {
             return false;
         }
 
+        private long getTimeMs() {
+            return (long)(DateTime.Now.ToUniversalTime() - new DateTime(1970, 1, 1)).TotalMilliseconds;
+        }
+
+        private MyGps createGps(int index, MyCubeGrid grid, Color gpsColor, long seconds) {
+
+            MyGps gps = new MyGps();
+            gps.Coords = grid.PositionComp.GetPosition();
+            gps.Name = "Top Grid - " + grid.DisplayName + " " + seconds;
+            gps.DisplayName = "Top Grid: " + grid.DisplayName;
+            gps.Description = ($"Top Grid: Grid currently in Top {index} by {Plugin.GpsIdentifierName}");
+            gps.GPSColor = gpsColor;
+            gps.IsContainerGPS = true;
+            gps.ShowOnHud = true;
+            gps.DiscardAt = new TimeSpan?();
+            gps.UpdateHash();
+
+            return gps;
+        }
+
+        public static MyIdentity GetIdentityById(long playerId) {
+
+            foreach (var identity in MySession.Static.Players.GetAllIdentities())
+                if (identity.IdentityId == playerId)
+                    return (MyIdentity)identity;
+
+            return null;
+        }
+
         public static MyPlayer GetPlayerById(long id) {
 
             if (MySession.Static.Players.TryGetPlayerId(id, out MyPlayer.PlayerId playerId))
@@ -289,6 +386,20 @@ namespace ALE_Biggest_Grids_Broadcast {
                     return player;
 
             return null;
+        }
+
+        public static string GetPlayerNameById(long playerId) {
+
+            MyIdentity identity = GetIdentityById(playerId);
+
+            if (identity != null)
+                return identity.DisplayName;
+
+            return "Nobody";
+        }
+
+        public static IMyFaction GetFactionForPlayer(long playerId) {
+            return MySession.Static.Factions.TryGetPlayerFaction(playerId);
         }
     }
 }
--- GridsBroadcastPlugin.cs
+++ GridsBroadcastPlugin.cs
@@ -98,7 +98,6 @@ namespace ALE_Biggest_Grids_Broadcast
                     } else {
                         Log.Warn("No multiplayer manager loaded!");
                     }
-                        
 
                     removeGpsFromAllPlayers();
 
