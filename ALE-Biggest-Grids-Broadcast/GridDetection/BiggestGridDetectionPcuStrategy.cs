a325d09a82fa7dfb4915b460a61eed7593e24550
TheCrunchy <crunchycamsta@gmail.com>
2021-04-03 17:31:01 +0100

    Should be final edits

--- ALE-Biggest-Grids-Broadcast/GridDetection/BiggestGridDetectionPcuStrategy.cs
+++ ALE-Biggest-Grids-Broadcast/GridDetection/BiggestGridDetectionPcuStrategy.cs
@@ -55,20 +55,17 @@ namespace ALE_Biggest_Grids_Broadcast.GridDetection {
         }
         private long CountProjectionPCU(MyCubeGrid grid) {
             long pcu = 0;
-            //loop over the blocks in the grid
-            foreach (MySlimBlock block in grid.GetBlocks()) {
-                //if its fatblock isnt null and is a projector count its pcu
-                if (block.FatBlock != null && block.FatBlock is MyProjectorBase projector) {
-                    //if the projector is enabled and the entity id of the grid its on matches the one we are checking, count the pcu
-                    if (projector.CubeGrid.EntityId == grid.EntityId && projector.Enabled) {
-                        List<MyCubeGrid> grids = projector.Clipboard.PreviewGrids;
-                        foreach (MyCubeGrid CubeGrid in grids) {
-                            pcu += CubeGrid.CubeBlocks.Count;
-                        }
+             /*loop over the projectors in the grid */
+            foreach (var projector in grid.GetFatBlocks().OfType<MyProjectorBase>()) {
+                 /*if the projector is enabled and the entity id of the grid its on matches the one we are checking, count the projected blocks */
+                if (projector.CubeGrid.EntityId == grid.EntityId && projector.Enabled) {
+                    List<MyCubeGrid> grids = projector.Clipboard.PreviewGrids;
+                    foreach (MyCubeGrid CubeGrid in grids) {
+                        pcu += CubeGrid.CubeBlocks.Count;
                     }
                 }
             }
-        
+
             return pcu;
         }
         private KeyValuePair<long, List<MyCubeGrid>> CheckGroupsPcu(HashSetReader<MyGroups<MyCubeGrid, MyGridMechanicalGroupData>.Node> nodes, GridsBroadcastConfig config) {
